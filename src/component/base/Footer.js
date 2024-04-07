8d73e13e5cf2f6e294bf794874d3147bd55ec90c
Juki0204 <jukid0204@yahoo.co.jp>
2024-04-07 16:24:04 +0900

    RSSフィードの実装＋軽微な修正

--- src/component/base/Footer.js
+++ src/component/base/Footer.js
@@ -7,7 +7,7 @@ import { css } from "@emotion/react";
 
 const footer = css`
     width: 100%;
-    background: #ccc;   
+    background: #ccc;
     padding-top: 10px;
 `;
 
@@ -23,6 +23,16 @@ export default function Footer(){
                 <ListItem marginInline='10px'>
                     <Link href="https://github.com/Juki0204">
                         <Image src="/img/GitHub_icon.png" width={32} height={32} alt="GitHub" />
+                    </Link>
+                </ListItem>
+                <ListItem marginInline='10px'>
+                    <Link href="/feed">
+                        <Image src="/img/RSS_icon.png" width={32} height={32} alt="RSS" />
+                    </Link>
+                </ListItem>
+                <ListItem display='flex' alignItems='end'>
+                    <Link href="http://validator.w3.org/feed/check.cgi?url=https%3A//naomaru-blog.vercel.app/feed">
+                        <Image src="/img/valid-rss-rogers.png" alt="[Valid RSS]" width={66} height={24} title="Validate my RSS feed" />
                     </Link>
                 </ListItem>
             </List>
